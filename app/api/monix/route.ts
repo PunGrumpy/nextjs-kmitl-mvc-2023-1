90acfb8e6b893e44f89f7a3f509b6ae901b46dba
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-09 20:31:49 +0700

    fix(api): update `GET`

--- app/api/monix/route.ts
+++ app/api/monix/route.ts
@@ -6,40 +6,17 @@ import { CalculateAverage, CalculatePercentile } from '@/lib/util'
 /**
  * @swagger
  * /api/monix:
- *  post:
- *    description: Post a new time and integer value
- *    requestBody:
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/TimeData'
- *          example:
- *            time: 2021-08-01T00:00:00.000Z
- *            integerValue: 1
- *    responses:
- *      201:
- *        description: Returns the average, 50th, 90th and 95th percentile of the integer values within the last hour
- *      400:
- *        description: Please provide time and integerValue
- *      404:
- *        description: No integer values found
- *      500:
- *        description: Internal server error
+ *  get:
+ *   description: Get the average, 50th, 90th, and 95th percentile of the last hour of data
+ *   responses:
+ *    200:
+ *      description: Returns the average, 50th, 90th, and 95th percentile of the last hour of data
+ *    404:
+ *      description: No data found
+ *    500:
+ *      description: Internal server error
  */
-export async function POST(request: Request) {
-  const { time, integerValue }: TimeData = await request.json()
-
-  if (!time || !integerValue) {
-    return ResponseJSON({ error: 'Please provide time and integerValue' }, 400)
-  }
-
-  await prisma.timeData.create({
-    data: {
-      time,
-      integerValue
-    }
-  })
-
+export async function GET() {
   const timeDataWithinOneHour = await prisma.timeData.findMany({
     where: {
       time: {
@@ -61,13 +38,55 @@ export async function POST(request: Request) {
 
   return ResponseJSON(
     {
-      result: {
-        average,
-        'Percentile 50th': percentile50,
-        'Percentile 90th': percentile90,
-        'Percentile 95th': percentile95
-      },
-      message: 'Successfully added new time and integer value'
+      average,
+      percentile50,
+      percentile90,
+      percentile95
+    },
+    200
+  )
+}
+
+/**
+ * @swagger
+ * /api/monix:
+ *  post:
+ *    description: Post a new time and integer value
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/TimeData'
+ *          example:
+ *            time: 2021-08-01T00:00:00.000Z
+ *            integerValue: 1
+ *    responses:
+ *      201:
+ *        description: Returns the time and integer value that was created
+ *      400:
+ *        description: Please provide time and integerValue
+ *      404:
+ *        description: No integer values found
+ *      500:
+ *        description: Internal server error
+ */
+export async function POST(request: Request) {
+  const { time, integerValue }: TimeData = await request.json()
+
+  if (!time || !integerValue) {
+    return ResponseJSON({ error: 'Please provide time and integerValue' }, 400)
+  }
+
+  const timeData = await prisma.timeData.create({
+    data: {
+      time,
+      integerValue
+    }
+  })
+
+  return ResponseJSON(
+    {
+      timeData
     },
     201
   )
