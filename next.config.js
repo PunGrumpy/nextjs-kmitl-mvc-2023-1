009e6d1476915d8dd58ee0714aa13dafe22a1692
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-10 18:40:12 +0700

    fix(*): try to fix for deploy to vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    serverlessTarget: {
+        target: 'experimental-serverless-trace'
+    },
+    webpack: (config) => {
+        config.resolve.alias['@prisma/client'] = PrismaClient
+        return config
+    }
+}
 
 module.exports = nextConfig
