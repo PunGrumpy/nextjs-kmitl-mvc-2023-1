cf346a7387f6d2f815c35256700477e4fc309e59
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-11 16:10:17 +0700

    fix 🐛 (*): change div to react fragment

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import React from 'react'
 import Form from '@/components/Form.component'
 import Header from '@/components/Header.component'
 
 export default function Page() {
   return (
-    <div>
+    <React.Fragment>
       <Header />
       <Form />
-    </div>
+    </React.Fragment>
   )
 }
